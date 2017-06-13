abfae7a534c11313ac11241a4c90fe9e4625b826
Alejandro Macías <alemacalc@gmail.com>
2017-06-13 12:15:40 +0200

    Terminada GUI
    ~ TODO:
      1. Controlador
      2. Buscar solución al DAO de préstamos
      3. Sacar el main() de Vista (ponerlo en App)

--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -6,10 +6,31 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JTable;
+import javax.swing.JInternalFrame;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import javax.swing.UIManager;
+import javax.swing.JSplitPane;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class Vista extends JFrame {
-
-	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -33,10 +54,103 @@ public class Vista extends JFrame {
 	public Vista() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnCargar = new JMenu("Cargar...");
+		menuBar.add(mnCargar);
+		
+		JMenuItem mntmLibros = new JMenuItem("... Libros");
+		mnCargar.add(mntmLibros);
+		
+		JMenuItem mntmEjemplares = new JMenuItem("... Ejemplares");
+		mnCargar.add(mntmEjemplares);
+		
+		JMenuItem mntmSocios = new JMenuItem("... Socios");
+		mnCargar.add(mntmSocios);
+		
+		JMenuItem mntmPrstamos = new JMenuItem("... Préstamos");
+		mnCargar.add(mntmPrstamos);
+		
+		JMenu mnAbout = new JMenu("About");
+		menuBar.add(mnAbout);
+		
+		JMenuItem mntmVersin = new JMenuItem("Versión");
+		mnAbout.add(mntmVersin);
+		
+		JMenuItem mntmCrditos = new JMenuItem("Créditos");
+		mnAbout.add(mntmCrditos);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		JButton btnAadir = new JButton("Añadir");
+		
+		JButton btnBorrar = new JButton("Borrar");
+		
+		JButton btnActualizar = new JButton("Actualizar");
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 202, GroupLayout.PREFERRED_SIZE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(48)
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+								.addComponent(textField, Alignment.LEADING)
+								.addComponent(textField_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
+								.addComponent(textField_2, Alignment.LEADING)
+								.addComponent(textField_3, Alignment.LEADING)))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(18)
+							.addComponent(btnAadir)
+							.addGap(18)
+							.addComponent(btnBorrar))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(55)
+							.addComponent(btnActualizar)))
+					.addGap(48))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(40)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 221, GroupLayout.PREFERRED_SIZE)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(32)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnAadir)
+								.addComponent(btnBorrar))
+							.addGap(18)
+							.addComponent(btnActualizar)))
+					.addGap(15))
+		);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		getContentPane().setLayout(groupLayout);
 	}
-
 }
